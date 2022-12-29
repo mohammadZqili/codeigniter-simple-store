7231b1cc14656b18d1e74a1e76c2ae9bb93d5d4d
mzukaili <ameenzqili@gmail.com>
2022-12-29 17:22:48 +0300

    init

---
+++ app/Filters/AccessFilter.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace App\Filters;
+
+use CodeIgniter\Filters\FilterInterface;
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class AccessFilter implements FilterInterface
+{
+    public function before(RequestInterface $request, $arguments = null)
+    {
+//        $auth = service('authentication');
+//
+//        if (! $auth->isLoggedIn()) {
+//            return redirect()->to(site_url('/login'));
+//        }
+    }
+
+    public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
+    {
+        // TODO: Implement after() method.
+    }
+}
